5639f95aa6f88e95a806df5a8486d9b6d427a9dd
zky339 <67003202+zky339@users.noreply.github.com>
2022-02-24 12:33:44 +0000

    Update GRPCClientService.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -44,4 +44,23 @@ public class GRPCClientService {
 		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
 		return resp;
     }
+    public String mult(){
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
+		.usePlaintext()
+		.build();
+		MatrixServiceGrpc.MatrixServiceBlockingStub stub
+		 = MatrixServiceGrpc.newBlockingStub(channel);
+		MatrixReply A=stub.multBlock(MatrixRequest.newBuilder()
+			.setA00(1)
+			.setA01(2)
+			.setA10(5)
+			.setA11(6)
+			.setB00(1)
+			.setB01(2)
+			.setB10(5)
+			.setB11(6)
+			.build());
+		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
+		return resp;
+    }
 }
